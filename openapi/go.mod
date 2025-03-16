36541481702ec2d6c13fcc82a39085a87701ce2c
Wonjinsin <slgktn92@gmail.com>
2025-03-16 21:51:08 +0900

    add. api template

---
+++ openapi/go.mod
@@ -0,0 +1,23 @@
+module github.com/wonjinsin/template/openapi
+
+go 1.23.2
+
+require (
+	github.com/labstack/echo/v4 v4.13.3
+	github.com/oapi-codegen/runtime v1.1.1
+)
+
+require (
+	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
+	github.com/google/uuid v1.5.0 // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
+	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	golang.org/x/time v0.8.0 // indirect
+)
--- openapi/main.go
+++ openapi/main.go
@@ -1,7 +1,101 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/wonjinsin/template/openapi/api"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Server struct {
+	// 여기에 필요한 의존성을 추가하세요
+	// 예: db *sql.DB
+}
+
+// ServerInterface 구현
+func (s *Server) GetUsers(c echo.Context) error {
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"resultCode": "200",
+		"resultData": []map[string]interface{}{
+			{
+				"id":    "1",
+				"name":  "Test User",
+				"email": "test@example.com",
+			},
+		},
+	})
+}
+
+func (s *Server) PostUsers(c echo.Context) error {
+	var reqBody api.PostUsersJSONRequestBody
+	if err := c.Bind(&reqBody); err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"resultCode": "200",
+		"resultData": map[string]interface{}{
+			"id":    "2",
+			"name":  reqBody.Name,
+			"email": reqBody.Email,
+		},
+	})
+}
+
+func (s *Server) GetUsersId(c echo.Context, id uint64) error {
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"resultCode": "200",
+		"resultData": map[string]interface{}{
+			"id":    fmt.Sprintf("%d", id),
+			"name":  "Test User",
+			"email": "test@example.com",
+		},
+	})
+}
+
+func (s *Server) PutUsersId(c echo.Context, id uint64) error {
+	var reqBody api.PutUsersIdJSONRequestBody
+	if err := c.Bind(&reqBody); err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"resultCode": "200",
+		"resultData": map[string]interface{}{
+			"id":    fmt.Sprintf("%d", id),
+			"name":  reqBody.Name,
+			"email": "test@example.com",
+		},
+	})
+}
+
+func (s *Server) DeleteUsersId(c echo.Context, id uint64) error {
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"resultCode": "200",
+		"resultMsg":  "User deleted successfully",
+	})
+}
 
 func main() {
-	fmt.Println("vim-go")
+	e := echo.New()
+
+	// 미들웨어 설정
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORS())
+
+	// 서버 인스턴스 생성
+	server := &Server{
+		// 여기에 필요한 의존성 초기화
+		// 예: db: database.Connect(),
+	}
+
+	// API 핸들러 등록
+	api.RegisterHandlers(e, server)
+
+	// 서버 시작
+	e.Logger.Fatal(e.Start(":8080"))
 }
